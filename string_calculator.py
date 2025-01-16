55f927935e90ebe4d1fc2a022da536f857d96274
Balaram <Balarams@regenesys.net>
2025-01-16 23:47:47 +0530

    Committing version 1.4 of tdd

--- string_calculator.py
+++ string_calculator.py
@@ -3,4 +3,11 @@
 def add(numbers: str) -> int:
     if numbers == "":
         return 0
+    # return sum(map(int, numbers.replace("\n", ",").split(',')))
+
+    if numbers.startswith("//"):
+        delimiter = numbers[2:numbers.index("\n")]
+        numbers = numbers[numbers.index("\n")+1:]
+        return sum(map(int, numbers.split(delimiter)))
     return sum(map(int, numbers.replace("\n", ",").split(',')))
+
